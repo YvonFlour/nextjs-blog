d23e16f5c4500affe4303ffa72867678d81e5702
Yvon Flour Alvin <yvonflouralvin@gmail.com>
2023-05-04 13:04:45 +0100

    Change Site Title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Your Name';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = "J'aide les PME et les entrepreurs à construire des site internet qui attire";
 
 export default function Layout({ children, home }) {
     return (
